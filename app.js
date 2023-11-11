7fc02c3a205092e2d6446d262580d9118080984a
Jonathan Lee <79165906+jsmlee123@users.noreply.github.com>
2023-11-11 18:52:50 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import Hello from "./hello.js"
+import Lab5 from "./lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+
+const app = express()
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+app.use(express.json());
+Lab5(app);
+Hello(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+app.listen(process.env.PORT || 4000);
